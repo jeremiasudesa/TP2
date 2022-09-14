f735e2e34fb00981dd4ebf9ac16fca867b6e977b
jereudesa <jfigueiredopaschmann@udesa.edu.ar>
2022-09-14 22:10:18 +0700

    interaction texts design added

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # TP2
 Segundo trabajo practico de Pensamiento Computacional - Carrera de Ingenieria en Inteligencia Artificial - Udesa
 En este trabajo desarrollamos un editor de texto con sugerencias basadas en la Edit Distance.
+Dependencias: Google Translate API python.
 Forma de Usarlo:
 [...] TODO
--- main.py
+++ main.py
@@ -1,6 +1,28 @@
+def get_interaction_language(len: str) -> str:
+    f = None
+    if (len == "en"):
+        f = open("textos/textos_interaccion_en.txt")
+    else:
+        f = open("textos/textos_interaccion_es.txt")
+    ret = f.read()
+    f.close()
+    return ret
+
+
+def handle_len_input(len: str) -> str:
+    # TODO implement check
+    return len
+
+
 def main():
-    print("initial commit")
+    #input / validation
+    len = input(
+        "choose interaction language // elegi lenguaje de interaccion (en, es): ")
+    len = handle_len_input(len)
+    # get interaction texts
+    itxt = get_interaction_language(len)
+    print(itxt)
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
