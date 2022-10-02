cf800cf5f1007f1201eaf98706e41602b39a8d9f
jeremiasudesa <jfigueiredopaschmann@udesa.edu.ar>
2022-10-02 12:15:20 -0300

    check limpio

--- main.py
+++ main.py
@@ -47,13 +47,16 @@ def fix_error(wrd: int, parts: list, bkt: text_suggestion.bk_tree, interaction_t
     return
 
 
+def third_language(lan: str):
+    front.easter_egg()
+    exit()
+
+
 def main():
     front.clear_terminal()
     # conseguir lenguaje
     lan = ask_lan_from_user()
-    if (lan == 3):
-        front.easter_egg()
-        exit()
+    third_language(lan)
     # conseguir textos de interaccion
     itxt = back.get_interaction_text_from_DB(lan)
     # conseguir diccionario
